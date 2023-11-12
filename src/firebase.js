f3b52ee2ec3556f762a90cfa7ff43196983e3065
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-11-12 14:10:42 +0200

    add firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNPgoSuYjyd_Ln5_vOp-EYdxKL0ovKuik",
+  authDomain: "foodflow-1a2eb.firebaseapp.com",
+  projectId: "foodflow-1a2eb",
+  storageBucket: "foodflow-1a2eb.appspot.com",
+  messagingSenderId: "323643009181",
+  appId: "1:323643009181:web:8e4835bbb9caace5b8cc74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
