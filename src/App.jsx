b01d7254286db6a904797ef62eef9c3fecfdffe8
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-11-12 14:48:46 +0200

    add Login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Login from "./components/Login"
 import Navigation from "./components/Navigation"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
     <Navigation/>
+    <Login/>
     </>
   )
 }
