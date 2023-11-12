8b65d7ef8e67f095b85e4c256272dac8f189479a
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-11-12 15:19:09 +0200

    add firebase to login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,44 @@
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import styles from './Login.module.css'
+import { useState } from 'react';
+import { app, auth } from '../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 const Login = () => {
+    const { email, setEmail } = useState('');
+    const { password, setPassword } = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                console.log(userCredential)
+                // ...
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
     return (
         <div className={styles['form-container']}>
-            <Form>
+            <Form onSubmit={signIn}>
                 <Form.Group className="mb-3" controlId="formGroupEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formGroupPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
                 </Form.Group>
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
             </Form>
         </div>
-            
+
     );
 }
 
