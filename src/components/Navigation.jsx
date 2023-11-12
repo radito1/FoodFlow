ec338951e9edfbb34c477717305aa1e702c325e7
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-11-12 13:28:37 +0200

    add Navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,38 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function Navigation() {
+    return (
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">FoodFlow</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">How it works</Nav.Link>
+                        <NavDropdown title="Categories" id="collapsible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Burger</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Meat
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Soups</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                See all 
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">Login</Nav.Link>
+                        <Nav.Link href="#memes">Register</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Navigation;
