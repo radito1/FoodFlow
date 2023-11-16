bf5600520ca7e14140b8e23e8f7ec60fadc156f9
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-11-16 16:43:49 +0200

    style home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,9 +2,14 @@ import './home.css';
 
 const Home = () => {
     return (
-        <div className="home-container">
-            <h1>Home Page</h1>
-        </div>
+        <>
+            <h1 className='heading'>HOME PAGE</h1>
+            <div className="home-container">
+                <div className='text-container'>
+                    <p>Welcome to Food Flow, your culinary haven for discovering and sharing delectable recipes! Immerse yourself in a world of flavors, where passionate home chefs and food enthusiasts come together. Explore a diverse array of mouthwatering recipes, from sizzling entrees to decadent desserts. Whether you're a seasoned chef or just starting your culinary journey, Food Flow is the perfect place to find inspiration, share your kitchen adventures, and connect with a community that shares your love for extraordinary cuisine. Join us and let the delicious journey begin</p>
+                </div>
+            </div>
+        </>
     );
 }
 
