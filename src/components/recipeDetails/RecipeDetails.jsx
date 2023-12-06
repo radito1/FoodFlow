e6133cb1f94f609b8d03300b968e14a7db46f716
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-12-06 14:36:15 +0200

    add functions to recipe details

--- src/components/recipeDetails/RecipeDetails.jsx
+++ src/components/recipeDetails/RecipeDetails.jsx
@@ -1,35 +1,37 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useReducer, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getDatabase, ref, onValue } from 'firebase/database';
+import { useForm, SubmitHandler, SubmitErrorHandler } from 'react-hook-form';
+import * as Yup from 'yup';
 
 import AuthContext from '../../contexts/authContext';
 import dataService from '../../services/dataService';
+import reducer from './commentReduce';
 import styles from './recipeDetails.module.css'
 
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
+import { Form } from 'react-bootstrap';
 import EditRecipeModal from '../editRecipe/EditRecipeModal';
 import ConfirmationModal from '../confirmationModal/ConfirmationModal';
+
+const commentSchema = Yup.object().shape({
+    comment: Yup.string().required('Comment is required'),
+});
 
 const RecipeDetails = () => {
     const { myRecipes, id } = useParams();
     const { isAuthenticated } = useContext(AuthContext);
     const [modalShow, setModalShow] = useState(false);
     const [secondModalShow, setSecondModalShow] = useState(false);
+    const [comments, dispatch] = useReducer(reducer, []);
     const [recipeData, setRecipeData] = useState({});
     const db = getDatabase();
     const navigate = useNavigate();
-
-    const deleteHandler = () => {
-        dataService.removeRecipe(id)
-            .then(() => {
-                navigate('/catalog/all-recipes');
-            })
-            .catch((error) => {
-                console.error('Error removing recipe:', error);
-            });
-    }
+    const { register, handleSubmit, setValue, formState: { errors } } = useForm < CommentForm > ({
+        resolver: yupResolver(commentSchema),
+    });
 
     useEffect(() => {
         if (!id) {
@@ -52,7 +54,43 @@ const RecipeDetails = () => {
         return () => {
             unsubscribe();
         };
-    }, [id])
+    }, [id]);
+
+    const addCommentHandler = async (values) => {
+        try {
+            const newComment = await commentService.create(id, values.comment);
+            newComment.owner = { email };
+
+            dispatch({
+                type: 'ADD_COMMENT',
+                payload: newComment,
+            });
+
+            // Clear the comment field after successful submission
+            setValue('comment', '');
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleInputChange = (e) => {
+        setValue('comment', e.target.value);
+    };
+
+    const deleteHandler = () => {
+        dataService.removeRecipe(id)
+            .then(() => {
+                navigate('/catalog/all-recipes');
+            })
+            .catch((error) => {
+                console.error('Error removing recipe:', error);
+            });
+    }
+
+    // const { values, onChange, onSubmit } = useForm(addCommentHandler, {
+    //     comment: '',
+    // });
+
     return (
         <div className={styles['recipeDetails-container']}>
             <Card style={{ width: '25rem' }}>
@@ -80,16 +118,19 @@ const RecipeDetails = () => {
             {isAuthenticated
                 ?
                 <article className="create-comment">
-                    <label htmlFor='comment'>Add new comment:</label>
-                    <form className="form" >
-                        <textarea id='comment' name="comment"placeholder="Comment......"></textarea>
-                        <input className="btn submit" type="submit" value="Add Comment" />
-                    </form>
+                    <Form onSubmit={handleSubmit(addCommentHandler)}>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Add Comment</Form.Label>
+                            <Form.Control as="textarea" rows={3} {...register('comment')} onChange={handleInputChange} />
+                            <Form.Text className="text-danger">{errors.comment?.message}</Form.Text>
+                            <Button variant="primary" type="submit">
+                                Add
+                            </Button>
+                        </Form.Group>
+                    </Form>
                 </article>
                 : ''
             }
-
-
 
             <EditRecipeModal
                 show={modalShow}
