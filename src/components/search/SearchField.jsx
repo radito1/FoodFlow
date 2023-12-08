ed349c425c6ab74d488e7bb385ac8d623aa3c94d
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-12-08 17:06:24 +0200

    fix search

--- src/components/search/SearchField.jsx
+++ src/components/search/SearchField.jsx
@@ -1,22 +1,20 @@
 import { useForm, Controller } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+
+import styles from './searchField.module.css';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useNavigate } from 'react-router-dom';
 
-const schema = yup.object().shape({
-    searchTerm: yup.string().required('Search cannot be empty!'),
-});
-
 const SearchField = () => {
     const navigate = useNavigate();
-    const { control,setValue, handleSubmit } = useForm({
-        resolver: yupResolver(schema),
-    });
+    const { control, setValue, handleSubmit } = useForm();
 
     const onSubmit = (data) => {
+        if (data.searchTerm === '') {
+            return
+        }
+
         navigate(`/search/${data.searchTerm}`);
         setValue('searchTerm', '');
     };
@@ -27,24 +25,18 @@ const SearchField = () => {
                 name="searchTerm"
                 control={control}
                 defaultValue=""
-                render={({ field, fieldState }) => (
+                render={({ field }) => (
                     <>
                         <Form.Control
                             type="search"
                             placeholder="Search"
-                            className={`${fieldState.invalid ? 'is-invalid' : ''}`}
                             aria-label="Search"
                             {...field}
                         />
-                        {fieldState.invalid && (
-                            <Form.Control.Feedback type="invalid">
-                                {fieldState?.error?.message}
-                            </Form.Control.Feedback>
-                        )}
                     </>
                 )}
             />
-            <Button type="submit" variant="outline-success">
+            <Button className={styles['search-button']} type="submit" variant="outline-success">
                 Search
             </Button>
         </Form>
